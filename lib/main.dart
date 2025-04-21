e6fe17d2221ce92e124c02520b9d7b00bb1588af
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-21 20:27:36 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:climate_flutter_app/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(), home: LoadingScreen());
+  }
+}
